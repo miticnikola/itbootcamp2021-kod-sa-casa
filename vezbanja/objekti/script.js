eb3c57df8c201455cff600bb715505fcfd9b0f9b
miticnikola <nikola92mitic@gmail.com>
2021-05-20 21:01:22 +0200

    8 i 9 zadatak -- objekti

--- vezbanja/objekti/script.js
+++ vezbanja/objekti/script.js
@@ -113,9 +113,29 @@ let dan = {
             }
         }
         return tropski;
+    },
+    // 8. Dan je nepovoljan ako je razlika između neka dva uzastopna merenja veća od 8 stepeni. Metod vraća true ukoliko je dan bio 
+// nepovoljan, u suprotnom vraća false.
+    nepovoljan: function(){
+        let temp = this.temperatura;
+        for(let i = 0; i < temp.length; i++){
+            if(Math.abs(temp[i] - temp[i + 1]) > 8){
+                return true;
+            }
+        }
+        return false;
+    },
+    // 9. Za dan se kaže da je neuobičajen ako je bilo kiše i ispod -5 stepeni, ili bilo oblačno i iznad 25 stepeni, ili je bilo i kišovito i oblačno i sunčano. Metod vraća true ukoliko je dan bio neuobičajen, u suprotnom vraća false.
+    neuobicajen: function(){
+        let temp = this.temperatura;
+        for(let i = 0; i < temp.length; i++){
+            if(temp[i] < -5 && this.kisa == true || this.oblacno == true && temp[i] > 25 || this.kisa == true && this.oblacno == true && this.sunce == true){
+                return true;
+        }
     }
-
-}
+    return false;
+    }
+};
 
 // 1. Određuje i vraća prosečnu temperaturu izmerenu tog dana.
 console.log(dan.prosecna());
@@ -135,3 +155,9 @@ console.log(dan.leden1());
 
 // 7. Za dan se smatra da je tropski ukoliko nijedna temperatura izmerena tog dana nije iznosila ispod 24 stepena. Metod vraća true ukoliko je dan bio tropski, u suprotnom vraća false.
 console.log(dan.tropski());
+
+// 8. Dan je nepovoljan ako je razlika između neka dva uzastopna merenja veća od 8 stepeni. Metod vraća true ukoliko je dan bio 
+// nepovoljan, u suprotnom vraća false.
+console.log(dan.nepovoljan());
+// 9. Za dan se kaže da je neuobičajen ako je bilo kiše i ispod -5 stepeni, ili bilo oblačno i iznad 25 stepeni, ili je bilo i kišovito i oblačno i sunčano. Metod vraća true ukoliko je dan bio neuobičajen, u suprotnom vraća false.
+console.log(dan.neuobicajen());
