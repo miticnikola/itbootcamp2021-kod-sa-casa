ae84b6c9c6b76a77c43c52ff00cb9de6449d34cb
miticnikola <nikola92mitic@gmail.com>
2021-06-07 22:48:27 +0200

    klase - zadatak pacijenti

--- vezbanja/klase_seteri_geteri/script.js
+++ vezbanja/klase_seteri_geteri/script.js
@@ -90,3 +90,83 @@ let film3 = new Film("Putopisi iz Narnije", "Neki tamo indijac", 2012);
 console.log(film1, film2, film3);
 
 
+// Kreirati klasu Pacijent koja od polja sadrži ime, visina i tezina. Od metoda sadrži:
+// Stampaj() koja ispisuje sve podatke o pacijentu,
+// Bmi(), koja vraća bmi vrednost pacijenta,
+// Kritican(), koja vraća true ukoliko je bmi pacijenta manji od 15 ili veći od 40, a u suprotnom vraća false.
+// Kreirati tri objekta ove klase i testirati metode.
+// U klasi Pacijent, dodati odgovarajuće getere i setere, s tim što je potrebno da se u odgovarajućim seterima proveri da li je visina između 0 i 250, a težina između 0 i 550.
+
+class Pacijent {
+    constructor(i, v, t){
+        this.ime = i;
+        this.visina = v;
+        this.tezina = t;
+    }
+    stampaj(){
+        console.log(`Ime: ${this._ime}, Visina: ${this._visina}, Tezina: ${this._tezina}`);
+    }
+
+    bmi(){
+        return (this._tezina / this._visina);
+    }
+    kritican(){
+        if(this.bmi() < 15 || this.bmi() > 40){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+// Seters
+    set ime(x){
+        this._ime = x;
+    }
+
+    set visina(x){
+        if(x > 0 && x < 2.5){
+            this._visina = x;
+        } else {
+            this._visina = x;
+        }
+    }
+
+    set tezina(x){
+        if(x > 0 && x < 550){
+            this._tezina = x;
+        } else {
+            this._tezina = x;
+        }
+    }
+// Geters
+    get ime(){
+        this._ime;
+    }
+
+    get visina(){
+        this._visina;
+    }
+
+    get tezina(){
+        this._tezina;
+    }
+}
+let pacijent1 = new Pacijent("Miladinka", 1.7, 65);
+let pacijent2 = new Pacijent("Stoja", 1.63, 71);
+let pacijent3 = new Pacijent("Camil", 2, 102);
+
+pacijent1.stampaj();
+console.log(pacijent1.bmi());
+console.log(pacijent1.kritican());
+
+pacijent2.stampaj();
+console.log(pacijent2.bmi());
+console.log(pacijent2.kritican());
+
+pacijent3.stampaj();
+console.log(pacijent3.bmi());
+console.log(pacijent3.kritican());
+
+console.log(pacijent3._ime);
+console.log(pacijent3._visina);
+console.log(pacijent3._tezina);
