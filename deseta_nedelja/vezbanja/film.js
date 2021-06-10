55828bb9cbeb7b5266bfad9372bca19f35828887
miticnikola <nikola92mitic@gmail.com>
2021-06-10 17:52:53 +0200

    vezbanje klasa

--- deseta_nedelja/vezbanja/film.js
+++ deseta_nedelja/vezbanja/film.js
@@ -5,13 +5,11 @@
 // U klasi Film, dodati po tri setera za sva polja, s tim da se u seteru za godinu izdanja proverava da li je godina veća od 1800.
 
 class Film {
-    constructor(n, r, g){
+    constructor(n, r, g, o){
         this.naslov = n;
         this.reziser = r;
         this.godinaIzdanja = g;
-    }
-    stampaj(){
-        console.log(`Naslov filma: ${this.naslov}, Reziser: ${this.reziser}, Godina izdanja: ${this.godinaIzdanja}`);
+        this.ocene = o;
     }
     
     // Seters
@@ -30,6 +28,10 @@ class Film {
             this._godinaIzdanja = 1800;
         }
     }
+
+    set ocene(niz){
+        this._ocene = niz;
+    }
     
     // Geters
     get naslov(){
@@ -44,16 +46,205 @@ class Film {
         return this._godinaIzdanja;
     }
 
+    get ocene(){
+        return this._ocene;
+    }
+
+    // Metode
+    stampaj(){
+        console.log(`Naslov filma: ${this.naslov}, Reziser: ${this.reziser}, Godina izdanja: ${this.godinaIzdanja}`);
+    }
+    prosek(){
+        let zbirOcena = 0;
+        for(let i = 0; i < this.ocene.length; i++){
+            zbirOcena += this.ocene[i];
+        }
+        return zbirOcena / this.ocene.length;
+    }
+
 }
 
-let film1 = new Film("Drakula", "Neki tamo reziser", 1987);
-let film2 = new Film("Film 2", "Neki tamo drugi reziser", 1707);
-let film3 = new Film("Film3", "Neki tamo treci reziser", 1997);
+// U klasi Film dodati polje ocene koje čini niz ocena koje su korisnici dali filmu
+// Kreirati niz od barem tri objekta klase Film
+// Napraviti metod prosek koji vraća prosečnu ocenu 
+
+// Napraviti funkciju vekFilmova kojoj se prosleđuje niz filmova i ceo broj (vek), a funkcija ispisuje samo one filmove koji su stvoreni u prosleđenom veku
+
+// Napraviti funkciju prosecnaOcena kojoj se prosleđuje niz filmova, a koja određuje i vraća  prosečnu ocenu svih filmova.
+
+
+let film1 = new Film("Drakula", "Neki tamo reziser", 1987, [9, 7, 9, 8.2, 6]);
+let film2 = new Film("Film 2", "Neki tamo drugi reziser", 1707, [5, 7, 9, 6]);
+let film3 = new Film("Film3", "Neki tamo treci reziser", 1997, [2, 5, 10]);
+
+
+let filmovi = [film1, film2, film3];
+
+// console.log(Math.round(film2.godinaIzdanja / 100));
+
+let vekFilmova = (niz, vek) => {
+    niz.forEach(film => {
+        // if(Math.ceil(film.godina/100) == vek) // ne stavljati round jer on poveca za 1 i pravi problem
+        if(film.godinaIzdanja > (vek - 1) * 100 && film.godinaIzdanja <= vek*100){
+            film.stampaj();
+        }
+    });
+}
+
+vekFilmova(filmovi, 20);
+
+// Napraviti funkciju prosecnaOcena kojoj se prosleđuje niz filmova, a koja određuje i vraća  prosečnu ocenu svih filmova.
+let prosecnaOcena = niz => {
+    let zbirOcena = 0;
+    let brOcena = 0;
+
+   for(let i = 0; i < niz.length; i++){
+       brOcena += niz[i].ocene.length;
+       
+       for(let j = 0; j < niz[i].ocene.length; j++){
+           zbirOcena += niz[i].ocene[j];
+        }
+    }
+    // console.log(brOcena);
+    // console.log(zbirOcena);
+
+    return zbirOcena / brOcena;
+
+    //==== Drugi nacin ======
+    // niz.forEach(film => {
+    //     film.ocene.forEach(ocena => {
+    //         zbirOcena += ocena;
+    //         brOcena++;
+    //     });
+    // });
+    // return zbirOcena / brOcena;
+}
+
+console.log(prosecnaOcena(filmovi));
+
+// Napraviti funkciju najboljeOcenjeni kojoj se prosleđuje niz filmova, a ona vraća najbolje ocenjeni film.
+// Napraviti funkciju osrednjiFilm kojoj se prosleđuje niz filmova a ona vraća film koji je najbliži prosečnoj oceni svih filmova.
+// Napraviti funkciju najmanjaOcenaNajboljeg kojoj se prosleđuje niz filmova a ona određuje najbolji film i ispisuje njegovu najslabiju ocenu.
+// Napisati funkciju najmanjaOcena kojoj se prosleđuje niz filmova, a koja vraća koja je najmanja ocena koju je bilo koji film dobio.
+
+// Najbolje ocenjeni:
+let najboljeOcenjeni = niz => {
+    let najbolji = niz[0];
+
+    niz.forEach(film => {
+        if(film.prosek() > najbolji.prosek()){
+            najbolji = film;
+        }
+    });
+    return najbolji;
+}
+
+najboljeOcenjeni(filmovi).stampaj(); // da ne bi izbacio log objekta, nego podatke o filmu
+// console.log(`Najbolje ocenjen film je ${najboljeOcenjenji(filmovi).stampaj()}`);
+
+
+// Napraviti funkciju osrednjiFilm kojoj se prosleđuje niz filmova a ona vraća film koji je najbliži prosečnoj oceni svih filmova.
+/*
+film1 -> 8.5
+film2 -> 7
+
+globalni -> 8
+
+Math.abs(film1 - globalni) = 8.5 - 8 = 0.5;
+Math.abs(film2 - globalni) = 7 - 8 = -1 tj 1, jer mu nalazimo apsolutnu vrednost;
+*/
+
+let osrednjiFilm = niz => {
+    let najbliziObj = niz[0]; //pretpostavka da je prvi film (objekat) najblizi generalnom proseku
+    let prosecnaGlobal = prosecnaOcena(niz);
+    let najbliziRazlika = Math.abs(najbliziObj.prosek() - prosecnaGlobal);
+
+    niz.forEach(film => {
+        let razlikaTekuceg = Math.abs(film.prosek - prosecnaGlobal);//koliko se tekuci film razlikuje od globalne prosecne ocene
+        if(najbliziRazlika > razlikaTekuceg) {
+            najbliziRazlika = razlikaTekuceg;
+            najbliziObj = film;
+        }
+    });
+    return najbliziObj;
+}
+console.log(osrednjiFilm(filmovi).naslov);
+
+filmovi.forEach(film => {
+    console.log(film.naslov, film.prosek());
+});
+
+
+// Napraviti funkciju najmanjaOcenaNajboljeg kojoj se prosleđuje niz filmova a ona određuje najbolji film i ispisuje njegovu najslabiju ocenu.
+let najmanjaOcenaNajboljeg = niz => {
+    let najboljaOcena = najboljeOcenjeni(niz);
+
+    let najmanjaOcena = najboljaOcena.ocene[0];
+
+    for(let i = 0; i < najboljaOcena.ocene.length; i++){
+        if(najmanjaOcena > najboljaOcena.ocene[i]){
+            najmanjaOcena = najboljaOcena.ocene[i];
+        }
+    }
+    return najmanjaOcena;
+}
+console.log(najmanjaOcenaNajboljeg(filmovi));
+
+
+// Napisati funkciju najmanjaOcena kojoj se prosleđuje niz filmova, a koja vraća koja je najmanja ocena koju je bilo koji film dobio.
+let najmanjaOcena = niz => {
+    let najmanja = niz[0].ocene[0];
+
+    for(let i = 0; i < niz.length; i++){
+        for(let j = 0; j < niz[i].ocene.length; j++){
+            if(najmanja > niz[i].ocene[j]){
+                najmanja = niz[i].ocene[j];
+            }
+        }
+    }
+    return najmanja;
+}
+
+console.log(najmanjaOcena(filmovi));
+
+// Napraviti funkciju iznadOcene kojoj se prosleđuje ocena i niz filmova, a ona vraća niz onih filmova koji su bolje ocenjeni od prosleđene ocene.
+let iznadOcene = (niz, ocena) => {
+    let boljeOcenjeni = [];
+
+    niz.forEach(film => {
+        if(film.prosek() > ocena){
+            boljeOcenjeni.push(film);
+        }
+    });
+    return boljeOcenjeni;
+}
+console.log(film1.prosek(), film2.prosek(), film3.prosek());
+console.log(iznadOcene(filmovi, 7));
+
+
+// Napisati funkciju najcescaOcena kojoj se prosleđuje niz ocena, a ona vraća ocenu koju su filmovi najčešće dobijali. 
+
+
+// Napisati funkciju iznadOceneNoviji kojoj se prosleđuje ocena i niz filmova  a koja treba da na ekranu da ispiše sve podatke o najnovijem filmu koji zadovoljava prosleđenu ocenu
+
+
+
+/////////////////
+console.log(film2.prosek());
 
 film1.stampaj();
 
 console.log(film1, film2, film3);
 console.log(film1._reziser);
+
+console.log(film2.ocene);
+
+
+
+
+
+
+
 
 // Kreirati klasu Pacijent koja od polja sadrži ime, visina i tezina. Od metoda sadrži:
 // Stampaj() koja ispisuje sve podatke o pacijentu,
