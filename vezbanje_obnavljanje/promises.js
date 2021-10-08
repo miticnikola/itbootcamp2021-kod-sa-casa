80a42a1d27ba64a36e2fda6e4b8cad095dc9f82b
miticnikola <nikola92mitic@gmail.com>
2021-10-08 22:37:31 +0200

    Fetching photos from JSON file - Making cards with img and title

--- vezbanje_obnavljanje/promises.js
+++ vezbanje_obnavljanje/promises.js
@@ -96,7 +96,7 @@ let promise3 = new Promise((resolve, reject) => {
     setTimeout(resolve, 100, 'foo');
 });
 
-console.log(promise2);
+// console.log(promise2);
 
 Promise.all([promise1, promise2, promise3]).then(success => {
     console.log(success);
@@ -105,21 +105,56 @@ Promise.all([promise1, promise2, promise3]).then(success => {
 
 // Make HTTP request == fetching api
 
+// Fetch title and id
 let api = fetch("https://jsonplaceholder.typicode.com/posts");
 
-console.log(api);
+// console.log(api);
 
 api
     .then(response => {
         return response.json();
     })
     .then(json => json.forEach(file => {
-        document.body.innerHTML += 
-        `
-        <p style="border: 1px solid green; margin: 10px; padding: 10px 5px;">
-            <span>${file.id})</span>
-            ${file.title}
-         </p>
-         `;
+        document.body.innerHTML +=
+            `
+    <p style="border: 1px solid green; margin: 10px; padding: 10px 5px;">
+        <span>${file.id})</span>
+        ${file.title}
+        </p>
+        `;
     }))
     .catch("ERROR: Something go wrong");
+
+
+// Fetch photos === Making cards with photos
+let imgs = fetch('https://jsonplaceholder.typicode.com/photos');
+
+imgs
+.then(response => response.json())
+.then(json =>
+    json.forEach(file => {
+        if (file.id < 20) {
+
+            let div = document.createElement('div');
+            // div.id = 'card';
+
+            div.setAttribute('style', 'border: 1px solid gray; text-align: center; color: red; width: 60%; margin: 0 auto;padding: 10px;margin-top: 10px; background-color: khaki;')
+
+            let title = document.createElement('h4');
+            // title.id = 'title';
+            title.innerText = `${file.title}`;
+
+            let image = document.createElement('img');
+            image.src = `${file.url}`;
+            image.setAttribute('style', 'width: 70%; border: 2px dotted aqua;')
+
+
+            div.appendChild(title);
+            div.appendChild(image);
+
+            document.body.appendChild(div);
+
+
+        }
+    })
+);
