4d182bd0d7b54dd79b24e87a37b470f17951d2a3
miticnikola <nikola92mitic@gmail.com>
2021-10-08 16:41:15 +0200

    from callback hell to promise

--- vezbanje_obnavljanje/promises.js
+++ vezbanje_obnavljanje/promises.js
@@ -14,3 +14,34 @@ p.then(message => {
 .catch(message => {
     console.log('Ovo je neuspesno: ' + message);
 });
+
+
+
+//Prevent Callback hall (Callback to promise)
+let userLeft = true;
+let userWatching = false;
+
+let watchTut = (callback, errorCallback) => {
+    if(userLeft){
+        errorCallback({
+            name: 'User left',
+            message: ':('
+        })
+    } else if(userWatching){
+        errorCallback({
+            name: 'User watch memes',
+            message: 'MemeMemeMemeMeme'
+
+        })
+    } else {
+        callback('Approved');
+    }
+};
+
+// Dve callback funkcije unutar funkcije watchTut
+watchTut((success) => {
+    console.log(`Success: ${success}`);
+},
+ (error) => {
+     console.log(`!!!Error!!! Name: ${error.name}, message: ${error.message}.`);
+ });
